6588d328ac743c05aef8323d4cdf7fad453d0a73
DevOse <abuluoisereme@gmail.com>
2022-12-08 20:50:16 -0800

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - Entry Point
  *
  * Return: Always 0 (Success) 
- */
-
-int main (void)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
