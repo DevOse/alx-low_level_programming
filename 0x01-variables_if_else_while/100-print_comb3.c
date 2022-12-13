ff163e3b69e71b323ab221215bd296a8a3416181
DevOse <abuluoisereme@gmail.com>
2022-12-12 19:34:54 -0800

    adv task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	int unit = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (unit = '0'; unit <= '0'; unit++)
+		{
+			if ((unit != tens) || (tens < unit))
+			{
+				putchar(tens);
+				putchar(unit);
+				if (unit < '9' && tens < '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
