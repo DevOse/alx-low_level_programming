3a599fc5cc1769a9200d45ac62e4e5931f3b70b3
DevOse <abuluoisereme@gmail.com>
2022-12-13 21:10:56 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets ten times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int abc;
+	int ten;
+
+	for (ten = 0; ten <= 10; ten++)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+		{
+			_putchar(abc);
+		}
+	}
+	_putchar('\n');
+}
