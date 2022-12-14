5a92d4be34e658ba2de577f78131c1f75413658a
DevOse <abuluoisereme@gmail.com>
2022-12-13 23:44:56 -0800

    edit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return ('+1');
+		return (+1);
 	}
 	else if (n < 0)
 	{
@@ -21,6 +21,6 @@ int print_sign(int n)
 	}
 	else
 	{_putchar ('0');
-	return ('0');
+	return (0);
 	}
 }
