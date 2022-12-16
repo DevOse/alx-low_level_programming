c8576d477ed8988d77df9ecdbaa714285ba47a79
DevOse <abuluoisereme@gmail.com>
2022-12-16 14:11:45 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,16 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	a = 0;
-
-	while (a < 10)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			_putchar((b%10)+48);
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
-		a++;
 	}
 }
