2dc6fe784b63a4816a4889a9f7287e4f91cc84a9
DevOse <abuluoisereme@gmail.com>
2022-12-16 19:38:19 -0800

    edit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,13 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (a % 3 == 0)
-		{
-			printf("Fizz ");
-		}
 		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else
 		{
