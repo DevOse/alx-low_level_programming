6c5a70d6ea318928fc5f4237893c02f029cff7cd
DevOse <abuluoisereme@gmail.com>
2022-12-19 20:56:10 -0800

    task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * puts2 -  prints every other character of a string
- * @str: char
- * Return: 0
- */
+* puts2 - function that prints a string, followed by a new line, to stdout
+* @str: pointer value
+* Return: Always 0 (Success)
+*/
 
 void puts2(char *str)
 {
-	int a = 0;
+	int i;
 
-	while (str[a] >= 0)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (a % 2 == 0)
-		{
-			_putchar(str[a]);
-		}
-		a++;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
