9dbbe9c37dfdbf4a1d80c0c6d4c96a5be0efa42c
DevOse <abuluoisereme@gmail.com>
2023-01-18 20:58:49 -0800

    task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @argc: no of arguments
+ * @argv: array
+ * Return: 0
+ */
+
+int main(void)
+{
+	int bytes, a;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char*)main;
+
+	for (a = 0; a < bytes; a++)
+	{
+		if (a == bytes - 1)
+		{
+			printf("&02hhx\n", arr[a]);
+			break;
+		}
+		printf("%02hhx", arr[a]);
+	}
+	return (0);
+}
