486e4e5d5ad0c0d44da8add4e3918153955175b0
DevOse <abuluoisereme@gmail.com>
2023-02-01 05:02:51 -0800

    solved task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,6 +2,8 @@
 
 /**
  * factorial - returns the factorial of a given number
+ * if n is lower than 0, return -1 to indicate an error
+ * Factorial of 0 is 1
  * @n: given number
  * Return: factorial of given number
  */
